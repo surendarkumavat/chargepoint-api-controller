2cd5a9ba20dcd54dd45f960b95a2ec7d101c5a31
surendarkumavat <kumavat_surendar@yahoo.com>
2026-02-11 18:14:53 +0100

    [suri]handle invalid input

--- src/main/kotlin/com/suri/chargepoint/Routing.kt
+++ src/main/kotlin/com/suri/chargepoint/Routing.kt
@@ -8,17 +8,42 @@ import com.suri.chargepoint.domain.chargingsession.worker.AsyncAuthServiceWorker
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 internal fun Application.configureRouting(repo: ChargingSessionRepository, worker: AsyncAuthServiceWorker) {
     install(ContentNegotiation) {
         json()
     }
     install(StatusPages) {
+        exception<BadRequestException> { call, cause ->
+            logger.info("Bad request caused by $cause")
+            call.respond(
+                status = HttpStatusCode.BadRequest,
+                message = ChargingSessionsPostDefaultResponse(
+                    type = "api/status-codes/bad-request",
+                    title = "Bad Request",
+                    status = 400,
+                    detail = "Provided Input is invalid. Please check the request and try again."
+                )
+            )
+        }
         exception<Throwable> { call, cause ->
-            call.respond(status = HttpStatusCode.InternalServerError, message = ChargingSessionsPostDefaultResponse(type = "api/status-codes/internal-server-error", title = "Internal Server Error", status = 500, detail = "$cause"))
+            logger.error(t = cause, msg = { "Internal Server Error occured" })
+            call.respond(
+                status = HttpStatusCode.InternalServerError,
+                message = ChargingSessionsPostDefaultResponse(
+                    type = "api/status-codes/internal-server-error",
+                    title = "Internal Server Error",
+                    status = 500,
+                    detail = "$cause"
+                )
+            )
         }
     }
     chargingSessionRoutes(
